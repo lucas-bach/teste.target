b485f0241b644169a197a7698dba3a5d8fb98adf
Lucas Bach <lborgesbach@gmail.com>
2024-03-10 11:45:11 -0300

    testes

---
+++ 02_fibonacci.py
@@ -0,0 +1,14 @@
+def verifica_fibonacci(numero):
+    a, b = 0, 1
+    while a < numero:
+        if a == numero:
+            return True
+        a, b = b, a + b
+    return False
+
+numero = int(input("Digite um número para verificar se pertence à sequência de Fibonacci: "))
+
+if verifica_fibonacci(numero):
+    print(f"O número {numero} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {numero} não pertence à sequência de Fibonacci.")
---
+++ 05_inverter_caracteres.py
@@ -0,0 +1,11 @@
+def inverter_string(string):
+    string_invertida = ""
+    for i in range(len(string) - 1, -1, -1):
+        string_invertida += string[i]
+    return string_invertida
+
+# Exemplo de utilização
+string_original = input("Digite uma string para inverter: ")
+string_invertida = inverter_string(string_original)
+print("String original:", string_original)
+print("String invertida:", string_invertida)
